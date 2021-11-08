9840b2ad0e34b87677aac1b84ed3aa639b48fab1
marloBarksdale <lyndenflood@gmail.com>
2021-11-07 21:56:32 -0500

    added form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className='header'>
+    <div className='container'>
       <h1>Enter your Information Here</h1>
+      <Form />
     </div>
   );
 }
